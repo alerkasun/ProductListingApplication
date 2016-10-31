58b14ca9df2e0ccfb7224938f60f716b341eb03b
alerksun <sandulovve@gmail.com>
2016-10-31 09:05:42 +0200

    Add tests for new validations

--- spec/controllers/api/v1/products_controller_spec.rb
+++ spec/controllers/api/v1/products_controller_spec.rb
@@ -42,5 +42,20 @@ describe Api::V1::ProductsController do
       expect(JSON.parse(response.body)).to eq(
         "errors" => {"name"=>["can't be blank"], "price"=>["is not a number", "can't be blank"]})
     end
+
+    it "should has 'is too long' message for name" do
+      post :create, name: (0...51).map { ('a'..'z').to_a[rand(26)] }.join,
+                    price: rand() * 100
+      expect(JSON.parse(response.body)).to eq(
+        "errors" => {"name"=>["is too long (maximum is 50 characters)"]})
+    end
+
+    it "should has 'is too long' message for description" do
+      post :create, description: (0...401).map { ('a'..'z').to_a[rand(26)] }.join,
+                    price: rand() * 100,
+                    name: FFaker::Product.product_name
+      expect(JSON.parse(response.body)).to eq(
+        "errors" => {"description"=>["is too long (maximum is 400 characters)"]})
+    end
   end
 end
