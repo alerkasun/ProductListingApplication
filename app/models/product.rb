c45915af060c5dc107ca686f7d96d69787465f5e
alerksun <sandulovve@gmail.com>
2016-10-31 08:46:37 +0200

    Add validations for description and name, remove extra css

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ActiveRecord::Base
-  validates :name, presence: true
-  validates :price, numericality: { greater_than_or_equal_to: 0 },
+  validates :name, length: { maximum: 50 }, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 1000000 },
                     presence: true
+  validates :description, length: { maximum: 400 }
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -8,10 +8,12 @@ describe Product do
   it { should respond_to(:description) }
   it { should respond_to(:price) }
 
-
   it { should validate_presence_of :name }
+  it { should validate_length_of(:name).is_at_most(50) }
   it { should validate_presence_of :price }
-  it { should validate_numericality_of(:price).is_greater_than_or_equal_to(0) }
+  it { should validate_numericality_of(:price).is_greater_than_or_equal_to(1) }
+  it { should validate_numericality_of(:price).is_less_than_or_equal_to(1000000) }
+  it { should validate_length_of(:description).is_at_most(400) }
   it { expect(Product).to respond_to :create! }
 
   it "return product object" do
